72005badaef80e3636c133ae7b4ab1fd0ff48572
Daniel Ressi <danielressi@Daniels-MacBook-Pro.local>
2025-04-03 13:05:28 +0200

    chore(src): note about devices

--- llm_podcast_explorer/streamlit_app.py
+++ llm_podcast_explorer/streamlit_app.py
@@ -407,7 +407,9 @@ def main(analyis_mode):
                     - Each point represents an episode and similar episodes are visualised closer to each other.
                     - Click on a point to show episode details.
 
-                    **Note:** All insights are generated automatically with AI and may contain inaccuracies.
+                    **Note:** 
+                    - All insights are generated automatically with AI and may contain inaccuracies.
+                    - For better user experience use a tablet, laptop or computer
                     """
                     )
                     
@@ -419,7 +421,9 @@ def main(analyis_mode):
                     - Select a cluster by double clicking the name on the legend.
                     - Click on a point to show episode details.
 
-                    **Note:** All insights are generated automatically with AI and may contain inaccuracies.
+                    **Note:** 
+                    - All insights are generated automatically with AI and may contain inaccuracies.
+                    - For better user experience use a tablet, laptop or computer.
                     """
 
         st.caption("✨ Leveraging AI to explore content instead of generating it ✨")
