cc92adfab61a8d5399accafd8da9d4993830a2de
Daniel Ressi <danielressi@Daniels-MacBook-Pro.local>
2025-05-23 23:30:13 +0200

    refactor(app): refactor streamlit app

--- llm_podcast_explorer/streamlit_app.py
+++ llm_podcast_explorer/streamlit_app.py
@@ -133,31 +133,23 @@ def on_select():
 
 
 def _init_sesion_state():
-    # Initialize session state variables
-    if "timeline_mode" not in st.session_state:
-        st.session_state.timeline_mode = False
-    if "podcast_query" not in st.session_state:
-        st.session_state.podcast_query = False
-    if "selected_podcast" not in st.session_state:
-        st.session_state.selected_podcast = None
-    if "checkpoint" not in st.session_state:
-        st.session_state.checkpoint = True
-    if "filtered_clusters" not in st.session_state:
-        st.session_state.filtered_clusters = {}
-    if "selected_category" not in st.session_state:
-        st.session_state.selected_category = ALL_KEY
-    if "selection_state" not in st.session_state:
-        st.session_state.selection_state = None
-    if "click_selection" not in st.session_state:
-        st.session_state.click_selection = False
-    if "click_reset" not in st.session_state:
-        st.session_state.click_reset = False
-    if "zoom_state" not in st.session_state:
-        st.session_state.zoom_state = None
-    if "major_categories" not in st.session_state:
-        st.session_state.major_categories = None
-    if "searched_episode" not in st.session_state:
-        st.session_state.searched_episode = None
+    defaults = {
+        "timeline_mode": False,
+        "podcast_query": False,
+        "selected_podcast": None,
+        "checkpoint": True,
+        "filtered_clusters": {},
+        "selected_category": ALL_KEY,
+        "selection_state": None,
+        "click_selection": False,
+        "click_reset": False,
+        "zoom_state": None,
+        "major_categories": None,
+        "searched_episode": None,
+    }
+
+    for key, default in defaults.items():
+        st.session_state.setdefault(key, default)
 
 
 def set_title_on_top(title):
@@ -176,13 +168,17 @@ def set_title_on_top(title):
     )
 
     st.markdown(
+    (
         f"""
         <h1 style="text-align: left; margin-top: 0;">
-            {title} <span style="font-size: 14px;font-weight: normal">by</span> <span style="font-size: 14px;">FeedPAM</span>
+            {title}
+            <span style="font-size: 14px; font-weight: normal">by</span>
+            <span style="font-size: 14px;">FeedPAM</span>
         </h1>
-        """,
-        unsafe_allow_html=True,
-    )
+        """
+    ),
+    unsafe_allow_html=True,
+)
 
 
 def reset_search():
@@ -205,14 +201,7 @@ def click_reset():
     st.session_state.click_reset = False
     st.session_state.searched_episode = None
 
-
-def main(analyis_mode, animation_mode=False):
-    title = "Podcasts | Explored"
-    st.set_page_config(page_title=title, layout="centered", initial_sidebar_state="expanded")
-    set_title_on_top(title)
-
-    _init_sesion_state()
-    podcasts = {p.stem: str(p) for p in CHECKPOINT_PATH.glob("*.json")}
+def set_podcast_from_query_params(podcasts):
     podcast_query = st.query_params.get("podcast", None)
     if podcast_query is not None:
         if podcast_query in PODCAST_QUERY_LOOKUP:
@@ -221,6 +210,203 @@ def main(analyis_mode, animation_mode=False):
         elif podcast_query.replace("-", " ") in podcasts:
             st.session_state.podcast_query = True
             st.session_state.selected_podcast = podcast_query.replace("-", " ")
+
+def render_intro_text(placeholder=None):
+    #with placeholder.container():
+    st.markdown("")
+    st.markdown("")
+    st.markdown("")
+    st.markdown(
+    """
+    #### Explore the Big Picture Behind Every Podcast
+
+    Podcasts are full of ideas, connections and themes, but they're not always easy to navigate.
+    **FeedPAM** helps you break down, explore, and visualize the hidden patterns inside your favorite shows.
+
+    ✨ Discover the core themes - See what a podcast is really about.
+
+    🔗 Follow the connections - Trace how episodes link together.
+
+    🚀 Find the best episodes - Get straight to the topics that matter to you.
+
+
+    🎧 Start Exploring Now!
+
+    """
+    )
+
+def show_infos():
+    if analyis_mode == "static":
+        st.info(
+            "This app is running with static data. " \
+            "Go to [Github](https://github.com/danielressi/llm-podcast-explorer) page for more options"
+        )
+    with st.sidebar:
+        info_placeholder = st.empty()
+        st.session_state.click_reset = False
+        with info_placeholder.container():
+            if st.session_state.click_selection and st.session_state.selection_state:
+                display_data = st.session_state.selection_state[0]["customdata"][-1]
+                format_dict_to_markdown(display_data)
+
+                st.session_state.click_selection = False
+
+            elif len(st.session_state.filtered_clusters) == 0:
+                st.write(
+                    """
+                **Tips:**
+                - Select a category to start exploring the themes and topics of the podcast
+                - Each point represents an episode and similar episodes are visualised closer to each other.
+                - Click on a point to show episode details.
+
+                **Note:**
+                - All insights are generated automatically with AI and may contain inaccuracies.
+                - For better user experience use a tablet, laptop or computer
+                """
+                )
+
+            else:
+                """
+                **Tips:**
+
+                - Enable/disable clusters by clicking the names in the legend.
+                - Select a cluster by double clicking the name on the legend.
+                - Click on a point to show episode details.
+
+                **Note:**
+                - All insights are generated automatically with AI and may contain inaccuracies.
+                - For better user experience use a tablet, laptop or computer.
+                """
+
+def show_social():
+
+        cap, social = st.columns([3, 1])
+        cap.caption("✨ Leveraging AI to explore content instead of generating it ✨")
+
+        with social:
+            social_media_links = [
+                "https://www.instagram.com/podcasts.explored/",
+            ]
+            social_media_icons = SocialMediaIcons(social_media_links)
+
+            social_media_icons.render(justify_content="end")
+
+            st.write(" ")
+
+def update_and_render_fig(base_fig, cluster_data, timeline):
+    fig = copy.deepcopy(base_fig)
+
+    with st.container():
+        updated_fig, zoom_state = update_figure(
+            fig,
+            st.session_state.selected_category,
+            st.session_state.filtered_clusters,
+            cluster_data,
+            timeline,
+            st.session_state.click_selection,
+            previous_zoom=st.session_state.zoom_state,
+            selection_state=st.session_state.selection_state,
+            animation_mode=animation_mode,
+        )
+
+        st.session_state.zoom_state = zoom_state
+
+        st.plotly_chart(
+            updated_fig,
+            use_container_width=True,
+            key="plotly_state",
+            selection_mode=("points",),
+            on_select=on_select,
+            autoplay=True,
+            config={
+                "scrollZoom": True,
+                "doubleClick": "reset+autosize",
+                "doubleClickDelay": 1000,
+                "displayModeBar": not animation_mode,
+                "toImageButtonOptions": {
+                    "format": "png",  # one of png, svg, jpeg, webp
+                    "filename": "network_view",
+                    "height": 500,
+                    "width": 700,
+                    "scale": 3,  # Multiply title/legend/axis/canvas sizes by this factor
+                },
+            },
+        )
+
+def st_category_selection(major_categories):
+    st.session_state.major_categories = major_categories
+    major_category_options = sorted(major_categories, key=lambda k: len(major_categories[k]), reverse=True)
+    with st.container():
+        selected_category = st.selectbox(
+            "Select a category:",
+            options=[ALL_KEY, *major_category_options],
+            key="category_selection",
+            index=0,
+            on_change=reset_search,
+        )
+    return selected_category
+
+def explore_analysed_episodes(analysed_episodes, timeline, animation_mode):
+    base_fig, cluster_data, episode_lookup = create_network_graph(
+            analysed_episodes, timeline, animation_mode=animation_mode
+        )
+
+    try:
+        major_categories = analysed_episodes["category_2_clusters"]
+        selected_category = st_category_selection(major_categories)
+
+        with st.sidebar:
+            search_episode = st.selectbox(
+                "Search episodes",
+                options=episode_lookup.keys(),
+                key="episode_selection",
+                on_change=reset_category_selection,
+                index=None,
+                placeholder="Search",
+            )
+
+        # Reset on double click
+        if st.session_state.click_reset:
+            click_reset()
+        elif not st.session_state.click_selection:
+            st.session_state.selected_category = selected_category
+            st.session_state.searched_episode = search_episode
+
+        if st.session_state.searched_episode is not None:
+            ep_data = episode_lookup[st.session_state.searched_episode]
+            category = ep_data["category"][0] if len(ep_data["category"]) > 0 else None
+            category_clusters = major_categories[category] if category is not None else ep_data["clusters"]
+
+            st.session_state.filtered_clusters = {
+                c: True if c in ep_data["clusters"] else "legendonly" for c in category_clusters
+            }
+
+            st.session_state.selection_state = [ep_data]
+            st.session_state.selected_category = ep_data["category"]
+            st.session_state.click_selection = True
+        # filtered_clusters already set on_select call
+        elif st.session_state.selected_category == ALL_KEY:
+            st.session_state.filtered_clusters = {}
+        else:
+            st.session_state.filtered_clusters = {
+                c: True for c in major_categories[st.session_state.selected_category]
+            }
+
+    except StopException:
+        st.session_state.click_selection = False
+        st.session_state.selected_category = ALL_KEY
+
+    update_and_render_fig(base_fig, cluster_data, timeline)
+
+def main(analyis_mode, animation_mode=False):
+    title = "Podcasts | Explored"
+    st.set_page_config(page_title=title, layout="centered", initial_sidebar_state="expanded")
+    set_title_on_top(title)
+
+    _init_sesion_state()
+    podcasts = {p.stem: str(p) for p in CHECKPOINT_PATH.glob("*.json")}
+    set_podcast_from_query_params(podcasts)
+
 
     if analyis_mode == "active" and not st.session_state.podcast_query:
         reset_disabled = False
@@ -261,186 +447,19 @@ def main(analyis_mode, animation_mode=False):
             if not st.session_state.click_reset:
                 st.session_state.click_reset = reset_view
 
-    select_box_placeholder = st.empty()
-    placeholder = st.empty()
+        timeline = st.toggle("Timline mode", value=st.session_state.timeline_mode, disabled=False)
+
+
 
     if st.session_state.selected_podcast is None:
-        with placeholder.container():
-            st.markdown("")
-            st.markdown("")
-            st.markdown("")
-            st.markdown(
-                """
-            #### Explore the Big Picture Behind Every Podcast
-
-            Podcasts are full of ideas, connections, and themes — but they’re not always easy to navigate. **FeedPAM** helps you break down, explore, and visualize the hidden patterns inside your favorite shows.
-
-            ✨ Discover the core themes – See what a podcast is really about.
-
-            🔗 Follow the connections – Trace how episodes link together.
-
-            🚀 Find the best episodes – Get straight to the topics that matter to you.
-
-
-            🎧 Start Exploring Now!
-
-            """
-            )
+        render_intro_text()
     else:
-        with st.sidebar:
-            timeline = st.toggle("Timline mode", value=st.session_state.timeline_mode, disabled=False)
 
-        base_fig, cluster_data, episode_lookup = create_network_graph(
-            analysed_episodes, timeline, animation_mode=animation_mode
-        )
+        explore_analysed_episodes(analysed_episodes, timeline, animation_mode)
 
-        try:
-            major_categories = analysed_episodes["category_2_clusters"]
-            st.session_state.major_categories = major_categories
-            category_options = [ALL_KEY] + sorted(major_categories, key=lambda k: len(major_categories[k]), reverse=True)  # list(major_categories.keys())
-            with select_box_placeholder.container():
-                selected_category = st.selectbox(
-                    "Select a category:",
-                    options=category_options,
-                    key="category_selection",
-                    index=0,
-                    on_change=reset_search,
-                )
+        show_infos()
+        show_social()
 
-            with st.sidebar:
-                search_episode = st.selectbox(
-                    "Search episodes",
-                    options=episode_lookup.keys(),
-                    key="episode_selection",
-                    on_change=reset_category_selection,
-                    index=None,
-                    placeholder="Search",
-                )
-
-            # Reset on double click
-            if st.session_state.click_reset:
-                click_reset()
-            elif not st.session_state.click_selection:
-                st.session_state.selected_category = selected_category
-                st.session_state.searched_episode = search_episode
-
-            if st.session_state.searched_episode is not None:
-                ep_data = episode_lookup[st.session_state.searched_episode]
-                category = ep_data["category"][0] if len(ep_data["category"]) > 0 else None
-                category_clusters = major_categories[category] if category is not None else ep_data["clusters"]
-
-                st.session_state.filtered_clusters = {
-                    c: True if c in ep_data["clusters"] else "legendonly" for c in category_clusters
-                }
-
-                st.session_state.selection_state = [ep_data]
-                st.session_state.selected_category = ep_data["category"]
-                st.session_state.click_selection = True
-            # filtered_clusters already set on_select call
-            elif st.session_state.selected_category == ALL_KEY:
-                st.session_state.filtered_clusters = {}
-            else:
-                st.session_state.filtered_clusters = {
-                    c: True for c in major_categories[st.session_state.selected_category]
-                }
-
-        except StopException:
-            st.session_state.click_selection = False
-            st.session_state.selected_category = ALL_KEY
-
-        fig = copy.deepcopy(base_fig)
-
-        with placeholder.container():
-            updated_fig, zoom_state = update_figure(
-                fig,
-                st.session_state.selected_category,
-                st.session_state.filtered_clusters,
-                cluster_data,
-                timeline,
-                st.session_state.click_selection,
-                previous_zoom=st.session_state.zoom_state,
-                selection_state=st.session_state.selection_state,
-                animation_mode=animation_mode,
-            )
-
-            st.session_state.zoom_state = zoom_state
-
-            st.plotly_chart(
-                updated_fig,
-                use_container_width=True,
-                key="plotly_state",
-                selection_mode=("points",),
-                on_select=on_select,
-                autoplay=True,
-                config={
-                    "scrollZoom": True,
-                    "doubleClick": "reset+autosize",
-                    "doubleClickDelay": 1000,
-                    "displayModeBar": not animation_mode,
-                    "toImageButtonOptions": {
-                        "format": "png",  # one of png, svg, jpeg, webp
-                        "filename": "network_view",
-                        "height": 500,
-                        "width": 700,
-                        "scale": 3,  # Multiply title/legend/axis/canvas sizes by this factor
-                    },
-                },
-            )
-
-        if analyis_mode == "static":
-            st.info(
-                "This app is running with static data. Go to [Github](https://github.com/danielressi/llm-podcast-explorer) page for more options"
-            )
-
-        with st.sidebar:
-            info_placeholder = st.empty()
-            st.session_state.click_reset = False
-            with info_placeholder.container():
-                if st.session_state.click_selection and st.session_state.selection_state:
-                    display_data = st.session_state.selection_state[0]["customdata"][-1]
-                    format_dict_to_markdown(display_data)
-
-                    st.session_state.click_selection = False
-
-                elif len(st.session_state.filtered_clusters) == 0:
-                    st.write(
-                        """
-                    **Tips:**
-                    - Select a category to start exploring the themes and topics of the podcast
-                    - Each point represents an episode and similar episodes are visualised closer to each other.
-                    - Click on a point to show episode details.
-
-                    **Note:**
-                    - All insights are generated automatically with AI and may contain inaccuracies.
-                    - For better user experience use a tablet, laptop or computer
-                    """
-                    )
-
-                else:
-                    """
-                    **Tips:**
-
-                    - Enable/disable clusters by clicking the names in the legend.
-                    - Select a cluster by double clicking the name on the legend.
-                    - Click on a point to show episode details.
-
-                    **Note:**
-                    - All insights are generated automatically with AI and may contain inaccuracies.
-                    - For better user experience use a tablet, laptop or computer.
-                    """
-
-        cap, social = st.columns([3, 1])
-        cap.caption("✨ Leveraging AI to explore content instead of generating it ✨")
-
-        with social:
-            social_media_links = [
-                "https://www.instagram.com/podcasts.explored/",
-            ]
-            social_media_icons = SocialMediaIcons(social_media_links)
-
-            social_media_icons.render(justify_content="end")
-
-            st.write(" ")
 
 
 if __name__ == "__main__":
