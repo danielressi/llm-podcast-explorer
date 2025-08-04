92ecc0809378014302728268cc27acd5ac09bb09
Daniel Ressi <danielressi@Daniels-MacBook-Pro.local>
2025-08-04 22:39:20 +0200

    chore(src): load from s3

--- llm_podcast_explorer/io_utils.py
+++ llm_podcast_explorer/io_utils.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import boto3
 
 
@@ -12,7 +13,27 @@ def write_to_r2(src_path: str, bucket, target_path: str):
     )
     client.upload_file(src_path, bucket, target_path)
 
+def download_s3_file(bucket, target_path: str, dest_path: str):
+    session = boto3.session.Session()
+    client = session.client(
+        service_name="s3",
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        endpoint_url=os.environ["AWS_ENDPOINT_URL"],
+    )
+    client.download_file(bucket, target_path, dest_path)
 
-def write_to_json(json_str: str, path: str):
-    with open(path, "w", encoding="utf-8") as f:
-        f.write(json_str)
+def get_podcasts_from_s3(bucket, prefix=""):
+    session = boto3.session.Session()
+    client = session.client(
+        service_name="s3",
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        endpoint_url=os.environ["AWS_ENDPOINT_URL"],
+    )
+    paginator = client.get_paginator("list_objects_v2")
+    files = []
+    for page in paginator.paginate(Bucket=bucket, Prefix=prefix):
+        for obj in page.get("Contents", []):
+            files.append(Path(obj["Key"]))
+    return files
--- llm_podcast_explorer/scheduled_runs.py
+++ llm_podcast_explorer/scheduled_runs.py
@@ -11,7 +11,8 @@ LOGGER = logging.getLogger(__name__)
 
 SCHEDULE = {"https://geschichten-aus-der-geschichte.podigee.io/feed/mp3": ["wednesday"],
             "https://podcasts.apple.com/us/podcast/99-invisible/id394775318": ["tuesday"],
-            "https://podcasts.apple.com/us/podcast/empire/id1639561921": ["tuesday", "thursday", "saturday"],
+            "https://podcasts.apple.com/us/podcast/empire/id1639561921": ["tuesday", "thursday"],
+            "https://podcasts.apple.com/nl/podcast/revisionist-history/id1119389968": ["thursday"]
             }
 
 
