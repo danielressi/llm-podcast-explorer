86c4a9554bcef2c068f6c445d55786a1733600a0
Daniel Ressi <danielressi@Daniels-MacBook-Pro.local>
2025-09-09 09:31:02 +0200

    chore(src): run all with new pod-link format

--- llm_podcast_explorer/scheduled_runs.py
+++ llm_podcast_explorer/scheduled_runs.py
@@ -10,13 +10,18 @@ from src.io_utils import write_to_json, write_to_r2
 from src.rss_feed_analyzer import RSSFeedAnalyzer
 
 LOGGER = logging.getLogger(__name__)
-
+RUN_ALL = True
 SCHEDULE = {"https://geschichten-aus-der-geschichte.podigee.io/feed/mp3": ["wednesday"],
-            "https://podcasts.apple.com/us/podcast/99-invisible/id394775318": ["tuesday"],
-            "https://podcasts.apple.com/us/podcast/empire/id1639561921": ["tuesday", "thursday"],
+           # "https://podcasts.apple.com/us/podcast/99-invisible/id394775318": ["tuesday"],
+            #"https://podcasts.apple.com/us/podcast/empire/id1639561921": ["tuesday", "thursday"],
             "https://podcasts.apple.com/nl/podcast/revisionist-history/id1119389968": ["thursday"],
             "https://podcasts.apple.com/nl/podcast/data-skeptic/id890348705": ["monday"],
-            "https://podcasts.apple.com/nl/podcast/wanging-on-with-graham-norton-and-maria-mcerlane/id1821737353": ["monday"]
+            "https://podcasts.apple.com/nl/podcast/wanging-on-with-graham-norton-and-maria-mcerlane/id1821737353": ["monday"],
+            "https://podcasts.apple.com/nl/podcast/comedy-bang-bang-the-podcast/id316045799?l=en-GB": ["monday"],
+            "https://podcasts.apple.com/nl/podcast/verbrechen/id1374777077": ["tuesday"],
+            "https://podcasts.apple.com/nl/podcast/zeit-wissen-woher-wei%C3%9Ft-du-das/id338219632": ["sunday"],
+            "https://podcasts.apple.com/nl/podcast/youre-dead-to-me/id1479973402": ["friday"],
+            "https://podcasts.apple.com/nl/podcast/the-atlas-obscura-podcast/id1555769970": ["saturday"]
             }
 
 
@@ -42,7 +47,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     for rss_url, days in SCHEDULE.items():
-        if datetime.now().strftime("%A").lower() in days:
+        if datetime.now().strftime("%A").lower() in days or RUN_ALL:
             LOGGER.info(f"Running scheduled analysis for {rss_url}")
 
 
